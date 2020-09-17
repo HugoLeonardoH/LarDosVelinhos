0002b522ea9aedcd6e307e9cffdc86c0551e0663
HugoLeonardoH <hugols@outlook.com.br>
2020-09-17 15:44:30 -0300

    Updates

---
+++ precache-manifest.6db560d918e3ac77150204b6546d8338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d4a3f4ca031f6f4ee05e35c18848b4",
+    "url": "/LarDosVelinhos/index.html"
+  },
+  {
+    "revision": "6d813351610802c23a46",
+    "url": "/LarDosVelinhos/static/css/main.33bc148b.chunk.css"
+  },
+  {
+    "revision": "9122cd88c3670a26d9c9",
+    "url": "/LarDosVelinhos/static/js/2.a183505f.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/LarDosVelinhos/static/js/2.a183505f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d813351610802c23a46",
+    "url": "/LarDosVelinhos/static/js/main.af3450e9.chunk.js"
+  },
+  {
+    "revision": "c8386f6a177a0d8d8202",
+    "url": "/LarDosVelinhos/static/js/runtime-main.11e6f483.js"
+  },
+  {
+    "revision": "40f438a1d250e92818d140b4b525c84a",
+    "url": "/LarDosVelinhos/static/media/logo.40f438a1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d58a7f792121b86cce122d577d25f1c.js"
+  "/LarDosVelinhos/precache-manifest.6db560d918e3ac77150204b6546d8338.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LarDosVelinhos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
